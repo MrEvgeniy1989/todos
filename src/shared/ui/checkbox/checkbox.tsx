116834f2c1ba2cc858a67abb15a5e198a2ff744d
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-12-13 20:40:13 +0300

    feat: I created a Checkbox component and styles for it.

---
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -0,0 +1,56 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+
+import * as CheckboxRadix from "@radix-ui/react-checkbox";
+import * as LabelRadix from "@radix-ui/react-label";
+
+import s from "./checkbox.module.scss";
+import { CheckboxIcon } from "../../assets/icons/checkbox-icon.tsx";
+
+export type CheckboxProps = {
+  checked: boolean;
+  className?: string;
+  classNameLabel?: string;
+  id?: string;
+  label?: string;
+  onCheckedChange: (checked: boolean) => void;
+} & ComponentPropsWithoutRef<typeof CheckboxRadix.Root>;
+
+export const Checkbox = forwardRef<
+  ElementRef<typeof CheckboxRadix.Root>,
+  CheckboxProps
+>(
+  (
+    {
+      checked,
+      className,
+      disabled,
+      id,
+      label,
+      classNameLabel,
+      onCheckedChange,
+      name,
+      ...restProps
+    },
+    ref,
+  ) => (
+    <div className={s.container}>
+      <LabelRadix.Root className={s.label} aria-disabled={disabled}>
+        <CheckboxRadix.Root
+          {...restProps}
+          checked={checked}
+          className={s.root + " " + (className ?? "")}
+          disabled={disabled}
+          name={name}
+          id={id}
+          onCheckedChange={onCheckedChange}
+          ref={ref}
+        >
+          <CheckboxRadix.Indicator className={s.indicator}>
+            <CheckboxIcon />
+          </CheckboxRadix.Indicator>
+        </CheckboxRadix.Root>
+        <span className={classNameLabel}>{label}</span>
+      </LabelRadix.Root>
+    </div>
+  ),
+);
