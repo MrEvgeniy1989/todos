8679892bd0507565c2aab68c1842fe6c2888436b
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-12-15 17:04:14 +0300

    feat: Added snowflakes.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,24 @@
+import { Snowfall } from "react-snowfall";
 import { ToastContainer } from "react-toastify";
 import { Todolist } from "../widgets/todolist/todolist.tsx";
 import s from "./app.module.scss";
 import "react-toastify/dist/ReactToastify.css";
+import snowflakeSrc from "../shared/assets/images/snowflake.png";
 
 export function App() {
+  const snowflake = new Image();
+  snowflake.src = snowflakeSrc;
+
   return (
     <div className={s.app}>
+      <Snowfall
+        color="red"
+        images={[snowflake]}
+        snowflakeCount={100}
+        speed={[0.02, 0.05]}
+        radius={[10.0, 15.0]}
+        wind={[-0.1, 0.1]}
+      />
       <Todolist />
       <ToastContainer
         autoClose={5000}
